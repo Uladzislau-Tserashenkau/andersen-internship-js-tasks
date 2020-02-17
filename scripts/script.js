7a505354bd1e3dfe4d10bd119b4f7e95f1dd9730
postulato <cosadesgo@gmail.com>
2020-02-17 15:06:16 +0300

    added another solution using closure

--- scripts/script.js
+++ scripts/script.js
@@ -29,3 +29,34 @@
 // console.log(a.getSize(), b.getSize());
 
 // ===========================================
+
+// Singleton (using closure)
+const Universe = (function () {
+  
+  let instance;
+
+  return class Un {
+    constructor() {
+      if (typeof instance === 'object') {
+        return instance;
+      }
+      this.size = 100;
+      instance = this;
+    }
+    getSize () {
+      return this.size;
+    }
+    setSize (amount) {
+      this.size = amount;
+    }
+  };
+} ());
+
+
+const a = new Universe();
+const b = new Universe();
+
+console.log(b===a);
+console.log(a.getSize(), b.getSize());
+a.setSize(300);
+console.log(a.getSize(), b.getSize());
